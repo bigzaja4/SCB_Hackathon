dffc32f4e960d151410439fae9810e0fd89ff8a5
divinize <patric_emmel@outlook.com>
2019-06-22 12:53:31 +0700

    [ADD] react-touch-carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Login from './views/Login'
+import Welcome from './views/Welcome'
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={Login} />
       <Route path="home" component={Login} />
+      {/* <Route path="welcome" component={Welcome} /> */}
     </Router>
   )
 }
--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Color } from '../utils'
+import TouchCarousel from 'react-touch-carousel'
 
 const Container = styled.div`
   background: linear-gradient(180deg, rgba(255, 255, 255, 0) 52.6%, ${() => Color.secondary} 100%), ${() => Color.primary};
 `
 
+const listOfData = [
+    {}
+  ]
 
 export default class extends Component {
   state = {
 
   }
 
+CarouselContainer =  (props) => {
+    // render the carousel structure
+  }
+  
+renderCard  = (index, modIndex, cursor) => {
+    const item = listOfData[modIndex]
+    // render the item
+  }
+  
   componentDidMount() {
     // MARK: Check Token
+    console.log("****** welcome developmetn")
   }
 
   render() {
     return (
       <Container>
-        <div>wtf</div>
+          <div>
+              welcome page
+          </div>
       </Container>
     )
   }
