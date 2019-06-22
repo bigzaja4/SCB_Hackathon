f1791505e8b3ea019e58b5473ab0ba09c1e61962
wirawat_roj <bigzaja4@hotmail.com>
2019-06-22 21:35:58 +0700

    [UPDATE] router

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const controller = require("../controllers/loginController");
+const loginController = require("../controllers/loginController");
+const registerController = require("../controllers/registerController");
+const transactionController = require("../controllers/transactionController");
 const router = express.Router();
 
-router.post("/",controller.loginMiddleware,controller.getLogin);
+router.post("/login",loginController.loginMiddleware,loginController.getLogin);
+router.post("/register",registerController.regisCon);
+router.get("/transaction",loginController.requireJWTAuth,transactionController.getTransByUsername);
 
 module.exports = router;
