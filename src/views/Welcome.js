92d7f2a874de43ee26ae80d74cd24cad18ac6ddf
divinize <patric_emmel@outlook.com>
2019-06-22 15:04:11 +0700

    Add infomation card

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Color } from '../utils'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import Welcomecard from '../components/Welcomecard'
 
 
 const Container = styled.div`
   background: linear-gradient(180deg, rgba(255, 255, 255, 0) 52.6%, ${() => Color.secondary} 100%), ${() => Color.primary};
+`
+
+const CardContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 66px;
 `
 
 export default class extends Component {
@@ -22,32 +29,10 @@ export default class extends Component {
 
     render() {
         return (
-            <Container>
-                <div className="Dashboard">
-                    <div className="DHeader">
-                        &nbsp;&nbsp;&nbsp;&nbsp;Dashboard
-                    </div>
-                </div>
-
-                <Card>
-                    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-                    <Card.Content>
-                        <Card.Header>Matthew</Card.Header>
-                        <Card.Meta>
-                            <span className='date'>Joined in 2015</span>
-                        </Card.Meta>
-                        <Card.Description>
-                            Matthew is a musician living in Nashville.
-                        </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <a>
-                            <Icon name='user' />
-                            22 Friends
-                        </a>
-                    </Card.Content>
-                </Card>
-
+            <Container style={{height:'100vh'}}>
+                <CardContainer>
+                   <Welcomecard />
+                </CardContainer>
             </Container>
         )
     }
