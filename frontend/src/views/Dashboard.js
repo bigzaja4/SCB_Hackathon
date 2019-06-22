fff13d2e1c982a16c1ab64ef845ecb8be1ea3b7a
dreamteam <dreamteam@dreamteams-MacBook-Pro.local>
2019-06-22 11:57:09 +0700

    ARTART

---
+++ frontend/src/views/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Dashboard.css';
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <div className="DHeader">
+          Dashboard
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
