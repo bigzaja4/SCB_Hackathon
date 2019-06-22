0896dd815b814696ba62d61d9543f32b9c0cd193
wirawat_roj <bigzaja4@hotmail.com>
2019-06-22 11:27:27 +0700

    [ADD] service DecryptQrCode to Get bill ID

---
+++ backend/services/decryptQRCode.js
@@ -0,0 +1,15 @@
+function decryptQrCode(fullyQr){
+    let prefix = fullyQr;
+    prefix = prefix.substr(4);
+    let length = (prefix.substr(0,2))*1;
+    prefix = prefix.substr(2);
+    let resultLength = (prefix.substr(0,length))*1;
+    prefix = prefix.substr(length);
+    let result = prefix.substr(0,resultLength);
+    return result;
+}
+
+module.exports = {
+    decryptQrCode
+
+}
