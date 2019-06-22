f90f84124146e06c40d5d3852785ed3b03f349d7
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-06-22 12:29:28 +0700

    redis

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+// const cookieParser = require("cookie-parser");
+// const session = require("express-session");
+// เพิ่มตัว RedisStore ขึ้นมาจะทำหน้าที่เก็บ session แทน express-session ที่ใช้ในเวลาปกตินั่นเอง
+// const RedisStore = require("connect-redis")(session);
+const redis = require("redis"); //เป็นตัวสำหรับเชื่อมต่อไปยัง host เครื่องนั้นๆ
 const app = express();
 
 const config = require("./config/config.js");
@@ -8,21 +13,50 @@ global.gConfig = config;
 
 const auth = require("./services/auth");
 
+const client = redis.createClient(
+  global.gConfig.redis_port,
+  global.gConfig.redis_host,
+  { password: global.gConfig.redis_host, ttl: 1 }
+);
+
+// app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(
+//   session({
+//     // เป็นการ config ให้ session ใช้ redis ซึ่งใช้แบบ client connect ก็ได้หรือจะ inject host, port เอง
+//     store: new RedisStore({
+//       client
+//     }),
+//     secret: "Hello My First Time To Start ;)",
+//     resave: true,
+//     saveUninitialized: true
+//   })
+// );
+
+
 
 app.get("/api", (req, res) => {
   let publicBattles = [1, 2, 3];
   res.json(publicBattles);
 });
 
-app.get("/login", async (req, res) => {
+app.get("/test", async (req, res) => {
   const data = await auth.login();
   console.log(data);
   res.send(data);
 });
 
 const PORT = global.gConfig.node_port || 3002;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server listening on port ${PORT}...`);
+  const data = await auth.login();
+  client.set(
+    "AccessToken",
+    data.data.accessToken,
+    "EX",
+    data.data.expiresAt,
+    redis.print
+  );
 });
--- backend/config/config.js
+++ backend/config/config.js
@@ -7,7 +7,10 @@ const config = {
     node_port: 3002,
     APIKEY: "l7a41bdd3c0da647b48e9ddbdd18693794",
     APISecret: "953577a2b6554e03bbb540b48ed46b5a",
-    requestUId: "dfsfsdfdsf"
+    requestUId: "dfsfsdfdsf",
+    redis_port: 6379,
+    redis_host: "scbdreamteam.tk",
+    redis_password: ""
   },
   testing: {
     config_id: "testing"
@@ -21,7 +24,10 @@ const config = {
     node_port: process.env.PORT || 3002,
     APIKEY: "l7a41bdd3c0da647b48e9ddbdd18693794",
     APISecret: "953577a2b6554e03bbb540b48ed46b5a",
-    requestUId: "dfsfsdfdsf"
+    requestUId: "dfsfsdfdsf",
+    redis_port: 6379,
+    redis_host: "scbdreamteam.tk",
+    redis_password: ""
   }
 };
 
