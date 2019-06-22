b629fc0a1ef7e765f1ec7d8c1bb6c50f5fda034e
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2019-06-22 17:52:15 +0700

    add Modal to project

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,5 +7,29 @@ export const Color = {
 }
 
 export const Container = {
-  borderRadius: 8,
+  borderRadius: 10,
+}
+
+export const Text = {
+  font: {
+    family: 'Kanit',
+    size: 18
+  }
+}
+
+export const bankNumberFormatter = (number, format) => {
+  const numberArray = `${number}`.split('')
+  let count = 0
+  const result = format.split('').map((char, index) => {
+    if (char === 'y') {
+      return numberArray[count++]
+    } else if (char === 'x') {
+      count++
+      return char
+    }
+    return char
+  })
+  console.log(numberArray)
+  console.log(result)
+  return result
 }
