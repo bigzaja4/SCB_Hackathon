d4f4380d6fec119971fd3fa2c7c3319e75719ae5
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2019-06-22 17:56:51 +0700

    slip

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 // เพิ่มตัว RedisStore ขึ้นมาจะทำหน้าที่เก็บ session แทน express-session ที่ใช้ในเวลาปกตินั่นเอง
 // const RedisStore = require("connect-redis")(session);
 const redis = require("redis"); //เป็นตัวสำหรับเชื่อมต่อไปยัง host เครื่องนั้นๆ
+const { promisify } = require("util");
 const app = express();
 
 const config = require("./config/config.js");
@@ -17,6 +18,8 @@ const client = redis.createClient(
   global.gConfig.redis_port,
   global.gConfig.redis_host
 );
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
 // app.use(cookieParser());
 app.use(cors());
@@ -34,20 +37,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   })
 // );
 
+async function checkAccessToken(req, res, next) {
+  const accessToken = await getAccessToken();
+  if (accessToken) {
+    // console.log(11111);
+    req.accessToken = accessToken;
+  } else {
+    // console.log(2222);
+    const data = await api.login();
+    const response = await setAccessToken(data);
+    if (response) {
+      req.accessToken = await getAccessToken();
+    } else {
+      res.status(500).send("internal error");
+    }
+  }
+  next();
+}
+
+app.use(checkAccessToken);
+
 app.get("/api", (req, res) => {
   let publicBattles = [1, 2, 3];
   res.json(publicBattles);
 });
 
-app.get("/test", async (req, res) => {
-  const data = await api.login();
-  const slip = await api.slipVerification(
-    "201906225f3GFy43Qeh1vHp",
-    data.data.tokenType + " " + data.data.accessToken
-  );
-  console.log(slip);
-
-  res.send(slip);
+app.get("/check-slip", async (req, res) => {
+  if (req.query.slipRef) {
+    const slip = await api.slipVerification(req.query.slipRef, req.accessToken);
+    console.log(slip);
+    res.send(slip);
+  } else {
+    res.status(400).send("need slipRef");
+  }
 });
 
 app.get("/qrcode", async (req, res) => {
@@ -64,15 +86,24 @@ app.post("/callback", (req, res) => {
   res.send(req.body);
 });
 
+async function setAccessToken({ data }) {
+  const res = await setAsync(
+    "AccessToken",
+    data.tokenType + " " + data.accessToken,
+    "EX",
+    data.expiresAt
+  );
+  return res;
+}
+
+async function getAccessToken() {
+  const res = await getAsync("AccessToken");
+  return res;
+}
+
 const PORT = global.gConfig.node_port || 3002;
 app.listen(PORT, async () => {
   console.log(`Server listening on port ${PORT}...`);
   const data = await api.login();
-  client.set(
-    "AccessToken",
-    data.data.tokenType + " " + data.data.accessToken,
-    "EX",
-    data.data.expiresAt,
-    redis.print
-  );
+  setAccessToken(data);
 });
