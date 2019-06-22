88fe405a5877f50f482e1f83ab2ac3a96f8ae444
dreamteam <dreamteam@dreamteams-MacBook-Pro.local>
2019-06-22 13:41:30 +0700

    resolve conflict

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import './Dashboard.css';
+import { Container, Row, Col, } from 'reactstrap';
+
 
 function Dashboard() {
   return (
     <div className="Dashboard">
 
       <div className="DHeader">
-          &nbsp;&nbsp;&nbsp;&nbsp;Dashboard
+        &nbsp;&nbsp;&nbsp;&nbsp;Dashboard
       </div>
 
       <div className="NameUser">
         <h1 className="subtitle is-3">
-        &nbsp;&nbsp;&nbsp;&nbsp;Hello, Mr.John
+          &nbsp;&nbsp;&nbsp;&nbsp;Hello, Mr.John
         </h1>
       </div>
 
-    <div className="TransactionBox">
-        <p>Amount&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Date</p>
-    </div>
+      <div className="TransactionBox">
+        <Row id="DRow">
+        <Col xs="4" sm="10">Amount</Col>
+          <Col xs="4" sm="1">|</Col>
+          <Col xs="4" sm="1">Date</Col>
+        </Row>
+      </div>
 
     </div>
   );
